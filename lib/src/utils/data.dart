9ea8b490b89a60e2b53ec189151edc4517e5cb89
chantelleNokue <chantellemabvura@gmail.com>
2025-07-11 13:51:54 +0200

    First Commit

---
+++ lib/src/utils/data.dart
@@ -0,0 +1,22 @@
+/// Supported mobile carrier prefixes in Zimbabwe.
+const Map<String, List<String>> kMobileCarriers = {
+  'econet': ['077', '078'],
+  'netone': ['071'],
+  'telecel': ['073'],
+};
+
+/// Supported landline area codes in Zimbabwe.
+const Map<String, String> kLandlineAreaCodes = {
+  'harare': '024',
+  'bulawayo': '029',
+  'gweru': '054',
+  'mutare': '020',
+  'masvingo': '039',
+  'kwekwe': '055',
+  'chinhoyi': '067',
+  'marondera': '065',
+  'bindura': '066',
+};
+
+/// Zimbabwe country code used for international formatting.
+const String kCountryCode = '263'; 
